b3a03c799f2c188391063af46f88300555534124
Askydev <devashishsharma2016@gmail.com>
2021-03-01 19:15:55 +0530

    data & images added

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Netflix UI',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.black,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(),
