5edbc1337e24a586861afa5f1392a285554ebc48
Askydev <devashishsharma2016@gmail.com>
2021-03-02 19:37:25 +0530

    Appbar-custom

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class CustomAppbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.orange,
+    );
   }
 }
