0b09715d04a4062b98d26a2d12140971414f9e56
Askydev <devashishsharma2016@gmail.com>
2021-03-02 19:42:18 +0530

    Appbar-custom

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/assets.dart';
 
 class CustomAppbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 24.0),
       color: Colors.orange,
+      child: SafeArea(
+        child: Row(
+          children: [
+            Image.asset(Assets.netflixLogo0),
+          ],
+        ),
+      ),
     );
   }
 }
