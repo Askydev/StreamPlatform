3c085d2c43fefa5d3c1509426384fc5bff5c71d0
Askydev <devashishsharma2016@gmail.com>
2021-03-04 23:25:27 +0530

    Header-content// verticalIcons

--- lib/widgets/vertical_icon.dart
+++ lib/widgets/vertical_icon.dart
@@ -5,9 +5,31 @@ class VerticalIconButton extends StatelessWidget {
   final String title;
   final Function onTap;
 
-  const VerticalIconButton({Key key, this.icon, this.title, this.onTap}) : super(key: key);
+  const VerticalIconButton({Key key, this.icon, this.title, this.onTap})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+          ),
+          const SizedBox(
+            height: 2.0,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
