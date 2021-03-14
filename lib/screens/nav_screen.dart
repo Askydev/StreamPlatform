50eb6303876690df9ec862ee14c2b76279078a01
Askydev <devashishsharma2016@gmail.com>
2021-03-14 22:08:11 +0530

    navscreen// bottomNavigation

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -28,6 +28,26 @@ class _NavScreenState extends State<NavScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        items: _icons.map((title, icon) => MapEntry(
+            title,
+            BottomNavigationBarItem(
+              icon: Icon(
+                icon,
+                size: 30.0,
+              ),
+              title: Text(title),
+            )))
+        .values.toList(),
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.white,
+        selectedFontSize: 11.0,
+        unselectedItemColor: Colors.grey,
+        unselectedFontSize: 11.0,
+        onTap: (index) => setState(()=>_currentIndex=index),
+      ),
     );
   }
 }
